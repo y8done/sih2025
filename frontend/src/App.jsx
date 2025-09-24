83e649cf8545754adaba20e3ebcd1d0ff0ad1cd0
jainam308 <moxit84@gmail.com>
2025-09-24 23:38:11 +0530

    component change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import LCAForm from './components/LCAForm'
+// import LCAForm from './components/LCAForm'
 import LCADashboard from './components/LCADashboard'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <LCADashboard />
+     <LCADashboard/>
     </>
   )
 }
